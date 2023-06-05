64957bd3c18dc980361862972315c73172c7c7bf
Marshi00 <112508169+Marshi00@users.noreply.github.com>
2023-06-05 10:27:33 -0400

    HT

--- 5 - Hash Table/EXERCISE-HT-Keys.py
+++ 5 - Hash Table/EXERCISE-HT-Keys.py
@@ -1,23 +1,23 @@
 class HashTable:
-    def __init__(self, size = 7):
+    def __init__(self, size=7):
         self.data_map = [None] * size
-      
+
     def __hash(self, key):
         my_hash = 0
         for letter in key:
             my_hash = (my_hash + ord(letter) * 23) % len(self.data_map)
-        return my_hash  
+        return my_hash
 
     def print_table(self):
-        for i, val in enumerate(self.data_map): 
+        for i, val in enumerate(self.data_map):
             print(i, ": ", val)
-    
+
     def set_item(self, key, value):
         index = self.__hash(key)
-        if self.data_map[index] == None:
+        if self.data_map[index] is None:
             self.data_map[index] = []
         self.data_map[index].append([key, value])
-    
+
     def get_item(self, key):
         index = self.__hash(key)
         if self.data_map[index] is not None:
@@ -26,15 +26,14 @@ class HashTable:
                     return self.data_map[index][i][1]
         return None
 
-    ## WRITE KEYS METHOD HERE ##
-    #                          #
-    #                          #
-    #                          #
-    #                          #
-    ############################
-        
+    def keys(self):
+        all_keys = []
+        for i in range(len(self.data_map)):
+            if self.data_map[i] is not None:
+                for j in range(len(self.data_map[i])):
+                    all_keys.append(self.data_map[i][j][0])
+        return all_keys
 
-        
 
 my_hash_table = HashTable()
 
@@ -43,8 +42,6 @@ my_hash_table.set_item('washers', 50)
 my_hash_table.set_item('lumber', 70)
 
 print(my_hash_table.keys())
-
-
 
 """
     EXPECTED OUTPUT:
